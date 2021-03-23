7241ab571e7c1d1edf3597d7c82cb9199eaf5e89
rakibulTipu <80535704+rakibulTipu@users.noreply.github.com>
2021-03-23 12:42:47 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// const toggle = document.querySelector("toggle");
+// const navigation = document.querySelector("navigation");
+
+// toggle.addEventListener("click", () => {
+//   toggle.classList.toggle("active");
+//   navigation.classList.toggle("active");
+// });
+
+const toggle = document.querySelector(".toggle");
+const navigation = document.querySelector(".navigation");
+
+toggle.addEventListener("click", () => {
+  toggle.classList.toggle("active");
+  navigation.classList.toggle("active");
+});
